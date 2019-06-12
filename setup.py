1fa19da4b39b5b816aa61a004b652cf1274a867f
bdpedigo <benjamindpedigo@gmail.com>
2019-06-12 14:44:00 -0400

    updating initials

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='Modeling the Drosophila larva connectome',
-    author='neurodata',
-    license='BSD-3',
+    version="0.1.0",
+    description="Modeling the Drosophila larva connectome",
+    author="Neurodata",
+    license="BSD-3",
 )
