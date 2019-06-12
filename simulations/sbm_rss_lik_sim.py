4fcbb46787368097dd3e1c3699eadb88443b0ae0
bdpedigo <benjamindpedigo@gmail.com>
2019-06-12 17:36:28 -0400

    add sbm sim

--- simulations/sbm_rss_lik_sim.py
+++ simulations/sbm_rss_lik_sim.py
@@ -1,32 +1,83 @@
 #%%
 from sacred import Experiment
 from sacred.observers import SlackObserver, FileStorageObserver
-from sacred import SETTINGS
+from src.utils import estimate_sbm, compute_rss, compute_log_lik, gen_B, gen_sbm
 
-SETTINGS
+import numpy as np
+
+from joblib import Parallel, delayed
 
 ex = Experiment("SBM model selection")
 
-# slack_obs = SlackObserver.from_config("slack.json")
-# ex.observers.append(slack_obs)
+slack_obs = SlackObserver.from_config("slack.json")
+ex.observers.append(slack_obs)
 ex.observers.append(FileStorageObserver.create(f"./simulations/runs/{__file__}"))
 
 
 @ex.config
 def my_config1():
-    a = 10  # noqa: F841
-    b = "test"  # noqa: F841
+    n_sims = 2  # noqa: F841
+    n_blocks_range = [1, 2, 3, 4, 5, 6, 7, 8]
+    n_verts_range = [100, 200, 300, 500, 1000]  # noqa: F841
+    # n_jobs = 50  # noqa: F841
+    n_block_try_range = list(range(1, 12))  # noqa: F841
+
+    # keep these the same
+    a = 0.1
+    b = 0.1
+    assortivity = 6  # how strong to make the block diagonal
+
+    B_mat = gen_B(n_blocks_range[-1], a=a, b=b, assortivity=assortivity)  # noqa: F841
+
+    # let Zhu/Ghodsie choose embedding dimension
+    n_components = None  # noqa: F841
 
 
 @ex.capture
-def print_a_and_b(a, b):
-    print("a =", a)
-    print("b =", b)
+def run_sim(
+    seed, n_blocks_range, n_verts_range, n_block_try_range, B_mat, n_components
+):
+    np.random.seed(seed)
+    sbm_score = np.zeros(
+        (len(n_blocks_range), len(n_verts_range), len(n_block_try_range))
+    )
+    sbm_n_params = np.zeros_like(sbm_score)
+    sbm_rss = np.zeros_like(sbm_score)
+
+    for j, n_blocks in enumerate(n_blocks_range):
+        B_mat_trunc = B_mat[:n_blocks, :n_blocks]
+        for k, n_verts in enumerate((n_verts_range)):
+            graph, labels = gen_sbm(n_verts, n_blocks, B_mat_trunc)
+            for l, n_block_try in enumerate(n_block_try_range):
+                estimator, n_params = estimate_sbm(
+                    graph, n_block_try, n_components=n_components, directed=False
+                )
+
+                rss = compute_rss(estimator, graph)
+                score = compute_log_lik(estimator, graph)
+                sbm_rss[j, k, l] = rss
+                sbm_score[j, k, l] = score
+                sbm_n_params[j, k, l] = n_params
+
+    out = {"sbm": {"rss": sbm_rss, "score": sbm_score, "n_params": sbm_n_params}}
+    return out
 
 
 @ex.automain
-def my_main(a, b):
-    print(a)
-    print(b)
-    return 1
+def main(
+    n_sims,
+    n_jobs,
+    n_blocks_range,
+    n_verts_range,
+    n_block_try_range,
+    B_mat,
+    n_components,
+):
+    seeds = np.random.randint(1e8, size=n_sims)
+
+    def run(seed):
+        # n_jobs -2 uses all but one cores
+        return run_sim(seed)
+
+    outs = Parallel(n_jobs=-2, verbose=40)(delayed(run)(seed) for seed in seeds)
 
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,15 +2,20 @@ import numpy as np
 from graspy.cluster import GaussianCluster
 from graspy.embed import AdjacencySpectralEmbed
 from graspy.models import RDPGEstimator, SBEstimator
-from graspy.simulations import p_from_latent, sample_edges
+from graspy.simulations import p_from_latent, sample_edges, sbm
 
 
 def estimate_assignments(graph, n_communities, n_components=None):
+    """Given a graph and n_comunities, sweeps over covariance structures
+    Not deterministic
+    Not using graph bic or mse to calculate best
+    """
     embed_graph = graph.copy()
     latent = AdjacencySpectralEmbed(n_components=n_components).fit_transform(
         embed_graph
     )
-    latent = np.concatenate(latent, axis=1)
+    if isinstance(latent, tuple):
+        latent = np.concatenate(latent, axis=1)
     gc = GaussianCluster(min_components=n_communities, max_components=n_communities)
     vertex_assignments = gc.fit_predict(latent)
     return (vertex_assignments, gc.model_._n_parameters())
@@ -24,11 +29,11 @@ def hardy_weinberg(theta):
     return np.array(hw).T
 
 
-def estimate_sbm(graph, n_communities, n_components=None):
+def estimate_sbm(graph, n_communities, n_components=None, directed=False):
     vertex_assignments, n_params = estimate_assignments(
         graph, n_communities, n_components
     )
-    estimator = SBEstimator(directed=False, loops=False)
+    estimator = SBEstimator(directed=directed, loops=False)
     estimator.fit(graph, y=vertex_assignments)
     return estimator, n_params
 
@@ -51,10 +56,37 @@ def gen_hw_graph(n_verts):
 
 
 def compute_rss(estimator, graph):
-    p_mat = estimator.p_mat_
+    """Computes RSS, matters whether the estimator is directed
+    
+    Parameters
+    ----------
+    estimator : graspy estimator object
+        [description]
+    graph : nparray
+        [description]
+    
+    Returns
+    -------
+    [type]
+        [description]
+    """
+    graph = graph.copy()
+    p_mat = estimator.p_mat_.copy()
+    if not estimator.directed:
+        inds = np.triu_indices_from(p_mat)
+        p_mat = p_mat[inds]
+        graph = graph[inds]
     diff = (p_mat - graph) ** 2
     rss = np.sum(diff)
     return rss
+
+
+def compute_mse(estimator, graph):
+    """
+    Matters whether the estimator is directed
+    """
+    rss = compute_rss(estimator, graph)
+    return rss / graph.size
 
 
 def compute_log_lik(estimator, graph, c=0):
@@ -70,3 +102,26 @@ def compute_log_lik(estimator, graph, c=0):
     failures = np.multiply((1 - p_mat), (1 - graph))
     likelihood = successes + failures
     return np.sum(np.log(likelihood))
+
+
+def _n_to_labels(n):
+    n_cumsum = n.cumsum()
+    labels = np.zeros(n.sum(), dtype=np.int64)
+    for i in range(1, len(n)):
+        labels[n_cumsum[i - 1] : n_cumsum[i]] = i
+    return labels
+
+
+def gen_B(n_blocks, a=0.1, b=0.2, assortivity=4):
+    B_mat = np.random.uniform(a, b, size=(n_blocks, n_blocks))
+    B_mat -= np.diag(np.diag(B_mat))
+    B_mat += np.diag(np.random.uniform(assortivity * a, assortivity * b, size=n_blocks))
+    return B_mat
+
+
+def gen_sbm(n_verts, n_blocks, B_mat):
+    ps = np.array(n_blocks * [1 / n_blocks])
+    n_vec = np.random.multinomial(n_verts, ps)
+    graph = sbm(n_vec, B_mat, directed=False, loops=False)
+    labels = _n_to_labels(n_vec)
+    return graph, labels
