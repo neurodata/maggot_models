d54d2f388c22cb9e3740026c2af09b52a552d70a
bdpedigo <benjamindpedigo@gmail.com>
2019-06-20 14:37:32 -0400

    does this work

--- simulations/sbm_rss_lik_sim.py
+++ simulations/sbm_rss_lik_sim.py
@@ -130,6 +130,6 @@ def main(
     for i, out in enumerate(outs):
         out["sim_ind"] = i
         master_out_df = master_out_df.append(out, ignore_index=True, sort=True)
-    file_obs = ex.observers[1]
+    # file_obs = ex.observers[1]
     save_obj(master_out_df, file_obs, "master_out_df")
     return 0
