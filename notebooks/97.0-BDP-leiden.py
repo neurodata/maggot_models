21f775b1eb729552d44b6b1bc82fad71f52163a2
bdpedigo <benjamindpedigo@gmail.com>
2020-03-02 10:30:06 +0000

    24 jobs

--- notebooks/97.0-BDP-leiden.py
+++ notebooks/97.0-BDP-leiden.py
@@ -119,7 +119,7 @@ for i, seed in enumerate(seeds):
 print("\n\n\n\n")
 print(f"Running {len(rep_params)} jobs in total")
 print("\n\n\n\n")
-outs = Parallel(n_jobs=2, verbose=10)(
+outs = Parallel(n_jobs=24, verbose=50)(
     delayed(run_experiment)(**p) for p in rep_params[:20]
 )
 partitions, modularities = list(zip(*outs))
