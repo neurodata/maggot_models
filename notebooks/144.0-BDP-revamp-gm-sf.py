d1119e1c5d02b81a3d51e22da37d410209c74d7e
bdpedigo <benjamindpedigo@gmail.com>
2020-05-03 00:23:15 -0400

    update params

--- notebooks/144.0-BDP-revamp-gm-sf.py
+++ notebooks/144.0-BDP-revamp-gm-sf.py
@@ -162,7 +162,7 @@ for alpha in alphas:
 
     def run_gmp(seed):
         np.random.seed(seed)
-        sgm = GraphMatch(n_init=1, init_method="rand")
+        sgm = GraphMatch(n_init=1, init_method="rand", max_iter=100, eps=0.05)
         sgm.fit(match_mat, adj)
         return sgm.score_, sgm.perm_inds_
 
