e5d0acef084bafefadd474a5f6a8279e7a569c1a
bdpedigo <benjamindpedigo@gmail.com>
2021-10-06 18:12:57 -0400

    add path plotting

--- experiments/plot_paths/plot_paths.py
+++ experiments/plot_paths/plot_paths.py
@@ -12,15 +12,24 @@ from giskard.plot import set_theme, soft_axis_off
 from matplotlib.collections import LineCollection
 from src.data import load_maggot_graph, load_palette
 from src.io import savefig
+from tqdm import tqdm
 
 t0 = time.time()
+
+np.random.seed(8888)
 
 HUE_KEY = "simple_group"
 ORDER_KEY = "sum_walk_sort"
 n_components = 10
 min_split = 32
-i = 6
-GROUP_KEY = f"dc_level_{i}_n_components={n_components}_min_split={min_split}"
+level = 7
+gap = 50  # 40
+width = 0.25
+alpha = 0.0075
+linewidth = 0.0075
+n_subsamples = 100000
+end = "RGN"
+GROUP_KEY = f"dc_level_{level}_n_components={n_components}_min_split={min_split}"
 
 set_theme()
 palette = load_palette()
@@ -43,35 +52,6 @@ def open_simple_paths(save_path):
     return recovered_stuff
 
 
-paths_loc = (
-    "maggot_models/experiments/plot_paths/data/all_paths_sens-to-dVNC_cutoff5.csv.gz"
-)
-
-file_name = os.path.basename(paths_loc)
-file_name = file_name.split(".")[0]
-paths = open_simple_paths(paths_loc)
-print(f"Number of paths: {len(paths)}")
-
-
-# def process_paths(paths):
-#     paths = list(map(str, paths))
-#     paths = list(set(paths))
-#     paths = [path.strip("[]") for path in paths]
-#     paths = [path.replace(" ", "") for path in paths]
-#     paths = [path.split(",") for path in paths]
-#     paths = [[int(node) for node in path] for path in paths]
-#     return paths
-
-
-# paths = process_paths(paths)
-# print(f"Number of paths after removing duplicates: {len(paths)}")
-
-max_hops = max(map(len, paths))
-print(f"Maximum path length: {max_hops}")
-
-#%%
-
-
 def sort_meta(meta, sort_class, sort_item=None, class_order=[]):
     meta = meta.copy()
     total_sort_by = []
@@ -90,14 +70,37 @@ def sort_meta(meta, sort_class, sort_item=None, class_order=[]):
     return meta
 
 
+def calc_bar_params(sizes, label, top, palette=None):
+    heights = sizes.loc[label]
+    offset = top
+    starts = heights.cumsum() - heights + offset
+    colors = np.vectorize(palette.get)(heights.index)
+    return heights, starts, colors
+
+
+paths_loc = (
+    f"maggot_models/experiments/plot_paths/data/all_paths_sens-to-{end}_cutoff6.csv.gz"
+)
+
+print("Loading paths...")
+
+# get paths
+file_name = os.path.basename(paths_loc)
+file_name = file_name.split(".")[0]
+paths = open_simple_paths(paths_loc)
+print(f"Number of paths: {len(paths)}")
+
+# count
+max_hops = max(map(len, paths))
+print(f"Maximum path length: {max_hops}")
+
+# sort
 meta = mg.nodes.copy()
 meta = meta[~meta[GROUP_KEY].isna()]
 print(f"Number of nodes: {len(meta)}")
-
 meta = sort_meta(meta, [GROUP_KEY, HUE_KEY], [ORDER_KEY], [ORDER_KEY])
 
-#%%
-
+# remove paths with references to unclustered nodes
 valid_paths = []
 for path in paths:
     add_path = True
@@ -107,35 +110,21 @@ for path in paths:
             continue
     if add_path:
         valid_paths.append(path)
-
 paths = valid_paths
-
 print(f"Number of paths after removing invalid paths: {len(paths)}")
-
-#%%
 
 
 sizes = meta.groupby([GROUP_KEY, HUE_KEY], sort=False).size()
-
 clusters = sizes.index.unique(level=0)
 
-gap = 50  # 40
+
 n_nodes = sizes.sum()
-width = 0.25
 fig, ax = plt.subplots(1, 1, figsize=(15, 10))
 y_max = n_nodes + gap * len(clusters)
 ax.set_xlim((-0.5, max_hops - 0.5))
 ax.set_ylim((y_max, 0))
 
-
-def calc_bar_params(sizes, label, top, palette=None):
-    heights = sizes.loc[label]
-    offset = top
-    starts = heights.cumsum() - heights + offset
-    colors = np.vectorize(palette.get)(heights.index)
-    return heights, starts, colors
-
-
+print("Drawing cluster bars...")
 # draw cluster bars
 mids = []
 mid_map = {}
@@ -167,33 +156,36 @@ for x in np.arange(max_hops):
             )
 
 
-alpha = 0.01
-linewidth = 0.01
-
-np.random.seed(8888)
-n_subsamples = 100000
 plot_inds = np.random.choice(len(paths), n_subsamples)
 
+print("Creating lines...")
 lines = []
-for i, path in enumerate(paths):
-    if i in plot_inds:
-        for hop, (source, target) in enumerate(nx.utils.pairwise(path)):
-            y1 = meta.loc[source, "adjusted_idx"]
-            y2 = meta.loc[target, "adjusted_idx"]
-            x1 = hop + width / 2
-            x2 = hop + 1 - width / 2
-            lines.append([[x1, y1], [x2, y2]])
+for i in tqdm(plot_inds):
+    path = paths[i]
+    for hop, (source, target) in enumerate(nx.utils.pairwise(path)):
+        y1 = meta.loc[source, "adjusted_idx"]
+        y2 = meta.loc[target, "adjusted_idx"]
+        x1 = hop + width / 2
+        x2 = hop + 1 - width / 2
+        lines.append([[x1, y1], [x2, y2]])
 
-
+print("Adding lines...")
 lc = LineCollection(lines, colors="black", linewidths=linewidth, alpha=alpha, zorder=-5)
 ax.add_collection(lc)
-
 
 soft_axis_off(ax)
 ax.set_xlabel("Hops", fontsize="xx-large")
 ax.set_xticks(np.arange(max_hops))
 ax.set_xticklabels(np.arange(max_hops) + 1, fontsize="x-large")
 
-stashfig(
-    f"paths-plot-n_subsamples={n_subsamples}-file={file_name}-linewidth={linewidth}-alpha={alpha}"
+print("Saving figure...")
+figname = (
+    f"paths-plot-n_subsamples={n_subsamples}-file={file_name}"
+    f"-linewidth={linewidth}-alpha={alpha}-level={level}"
 )
+stashfig(figname)
+
+
+#%%
+meta['dc_level_6_n_components=10_min_split=32'].nunique()
+# %%
